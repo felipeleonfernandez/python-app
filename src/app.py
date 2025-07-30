f70b36b7d290e4845785fa1b303a704434c3e950
felipeleonfernandez <felipeleonfernandez@gmail.com>
2025-07-30 14:46:41 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, Felipe!',
+        'message': 'You are doing great, Felipe2!',
         'deployed_on': 'kubernetes'
     })
 
